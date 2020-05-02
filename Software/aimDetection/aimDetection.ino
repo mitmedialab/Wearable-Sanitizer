370349b8628fe64213df47d95e4a739e1256fda8
shtarbanov <alims@media.mit.edu>
2020-05-02 02:12:32 -0400

    Update Aim Detection

--- Software/aimDetection/aimDetection.ino
+++ Software/aimDetection/aimDetection.ino
@@ -8,14 +8,21 @@
  *  TODO: If the spraying has occurred 
  * 
  */
-float x[20],y[20],z[20];
-float xSumOld,xSumNew, xAvgOld, xAvgNew;
-float ySumOld,ySumNew, yAvgOld, yAvgNew;
-float zSumOld,zSumNew, zAvgOld, zAvgNew;
+#define SAMPLES 10 //the number of samples over which we will average.
+#define SAMPLING_DELAY 50 //time between the consecutive SAMPLES in ms.
+#define AIM_THRESHOLD 0.2
+int acquisitionTime = 0; //updates whenver a new sample is obtained.
+
+float xOld[SAMPLES], xNew[SAMPLES]; 
+float yOld[SAMPLES], yNew[SAMPLES];
+float zOld[SAMPLES], zNew[SAMPLES];
+uint8_t iSample=0; //index
+float xAvgOld, xAvgNew;
+float yAvgOld, yAvgNew;
+float zAvgOld, zAvgNew;
 
 #include <Adafruit_CircuitPlayground.h>
 #define CLICKTHRESHHOLD 120 //for 8G, try 10-20. for 4G try 20-40. for 2G try 40-80
-
 
 void setup() {
   CircuitPlayground.begin();
@@ -32,50 +39,42 @@ void loop() {
   //suppose I have an array of 20 items and I keep adding adding new readings to it while replacing the old ones, 
   //one reading every 100ms. Then I average the last 10 values and the latest 10 values and compare them. 
   //How do I shift the items in the array by 1. 
-
-  xSumOld = 0;
-  xSumNew = 0;
-  for(int i=19; i>0; i--){
-    x[i]=x[i-1]; //shift each element by 1 position
-    (i>9) ? xSumOld+=x[i] : xSumNew+=x[i]; //get the sums of the old 10 and new 9 values.
-  }
-  x[0] = CircuitPlayground.motionX();  
-  xSumNew+=x[0]; //Add the 10th value of the new sum.
-  xAvgOld = xSumOld/10.0f;
-  xAvgNew = xSumNew/10.0f;
-/////////////////////////////////////////////
-  ySumOld = 0;
-  ySumNew = 0;
-  for(int i=19; i>0; i--){
-    y[i]=y[i-1]; //shift each element by 1 position
-    (i>9) ? ySumOld+=y[i] : ySumNew+=y[i]; //get the sums of the old 10 and new 9 values.
-  }
-  y[0] = CircuitPlayground.motionY();  
-  ySumNew+=y[0]; //Add the 10th value of the new sum.
-  yAvgOld = ySumOld/10.0f;
-  yAvgNew = ySumNew/10.0f;
-/////////////////////////////////////////////////////////////
-  zSumOld = 0;
-  zSumNew = 0;
-  for(int i=19; i>0; i--){
-    z[i]=z[i-1]; //shift each element by 1 position
-    (i>9) ? zSumOld+=z[i] : zSumNew+=z[i]; //get the sums of the old 10 and new 9 values.
-  }
-  z[0] = CircuitPlayground.motionZ();  
-  zSumNew+=z[0]; //Add the 10th value of the new sum.
-  zAvgOld = zSumOld/10.0f;
-  zAvgNew = zSumNew/10.0f;
-
-  //Calculate the average of the first 10 items, and the average of the last 10 items.
-  //If the two averages are different by more than XX, then don't do enything. Else, flash LEDs.
-
-  //oldAverage
-  if(abs(xAvgNew - xAvgOld) < 0.3 && abs(yAvgNew - yAvgOld) < 0.3 && abs(zAvgNew - zAvgOld) < 0.3){
-    Serial.println("Aiming");
-    makeCircle(1,40);
-  }
+  if(millis()-acquisitionTime >= SAMPLING_DELAY){
+    //Get a new Sammple:
+    float xSumOld = 0, xSumNew = 0, ySumOld = 0, ySumNew = 0, zSumOld = 0, zSumNew = 0;
+    acquisitionTime = millis();
+    xOld[iSample]=xNew[iSample];
+    xNew[iSample]=CircuitPlayground.motionX();
+    yOld[iSample]=yNew[iSample];
+    yNew[iSample]=CircuitPlayground.motionY();
+    zOld[iSample]=zNew[iSample];
+    zNew[iSample]=CircuitPlayground.motionZ();
+    iSample++;
+    if(iSample==SAMPLES) iSample=0;
   
-  delay(100);
+    //Calculate the averages
+    for(int i=0; i<9; i++){
+      xSumOld += xOld[i];
+      xSumNew += xNew[i];
+      ySumOld += yOld[i];
+      ySumNew += yNew[i];
+      zSumOld += zOld[i];
+      zSumNew += zNew[i];
+    }
+    xAvgOld = xSumOld/10.0f;
+    xAvgNew = xSumNew/10.0f;
+    yAvgOld = ySumOld/10.0f;
+    yAvgNew = ySumNew/10.0f;
+    zAvgOld = zSumOld/10.0f;
+    zAvgNew = zSumNew/10.0f;
+  
+    //If the two averages are different by more than XX, then don't do enything. Else, flash LEDs.
+    if(abs(xAvgNew - xAvgOld) < AIM_THRESHOLD && abs(yAvgNew - yAvgOld) < AIM_THRESHOLD && abs(zAvgNew - zAvgOld) < AIM_THRESHOLD){
+      Serial.println("Aiming");
+      Serial.println(abs(xAvgNew - xAvgOld));
+      makeCircle(1,30);
+    }
+  }
 }
 
 
